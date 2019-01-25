58101ab3a23db487c3dc5f3c987344d9e7eb5a74
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-25 01:59:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,6 +109,8 @@ def changing_alan
      alans_new_info = "GUI"
      
   programmer_hash[:alan_kay][:known_for] = "alans_new_info"
+  
+  programmer_hash
      
 end
 
@@ -130,5 +132,6 @@ def adding_to_dennis
      }
      
  programmer_hash[:dennis_ritchie][:languages] = "Assembly"
-
+ 
+programmer_hash
 end
