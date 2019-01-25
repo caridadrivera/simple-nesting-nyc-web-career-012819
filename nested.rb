527d5425f9a9ee5ac65459631ef684edfc0f89e8
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-25 01:43:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,22 +66,22 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-     programmer_hash["yukihiro_matsumoto"] = {}
+# 	programmer_hash = 
+# 		{
+#         :grace_hopper => {
+#           :known_for => "COBOL",
+#           :languages => ["COBOL", "FORTRAN"]
+#         },
+#         :alan_kay => {
+#           :known_for => "Object Orientation",
+#           :languages => ["Smalltalk", "LISP"]
+#         },
+#         :dennis_ritchie => {
+#           :known_for => "Unix",
+#           :languages => ["C"]
+#         }
+#     }
+#     programmer_hash["yukihiro_matsumoto"] = {}
      
 end
 
@@ -103,7 +103,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+    
+    programmer_hash[alan_kay] = alans_new_info
      
 end
 
